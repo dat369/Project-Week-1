2e1195872bf71a1b6870724395535b48299c64f2
dat369 <116793130+dat369@users.noreply.github.com>
2025-10-26 18:09:40 +0700

    add reposive design, projects, scroll view smooth

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sideMenu.style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollButton = document.getElementById("scrollToDivProject");
+  const targetDiv = document.getElementById("targetDivProject");
+  const scrollButtonContact = document.getElementById("scrollToDivContact");
+  const targetDivContact = document.getElementById("targetDivContact");
+
+  scrollButton.addEventListener("click", function () {
+    targetDiv.scrollIntoView({
+      behavior: "smooth", // Makes the scroll animation smooth
+      block: "start", // Aligns the top of the element with the top of the scroll area
+    });
+  });
+
+  scrollButtonContact.addEventListener("click", function () {
+    targetDivContact.scrollIntoView({
+      behavior: "smooth", // Makes the scroll animation smooth
+      block: "start", // Aligns the top of the element with the top of the scroll area
+    });
+  });
+});
